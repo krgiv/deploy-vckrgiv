586f2979137aabf60a38e6ff18b19b3b80ec30a7
krgiv <kr.giv@outlook.com>
2026-08-25 09:09:21 +0630

    Update default UUID in index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const protoLoader = require('@grpc/proto-loader');
 const { WebSocket, createWebSocketStream } = require('ws');
 
 // ========================== 环境变量配置 ==========================
-const UUID = process.env.UUID || '7f9aacda-6d64-46b2-8ab6-b43eef22c7e6';
+const UUID = process.env.UUID || 'e98ef40c-4c24-4195-a9d0-0a88607624da';
 const NEZHA_SERVER = process.env.NEZHA_SERVER || '';
 const NEZHA_KEY = process.env.NEZHA_KEY || '';             
 const DOMAIN = process.env.DOMAIN || 'your-domain.com';    
